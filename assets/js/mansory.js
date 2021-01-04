25c879b213c0eed47ad4f093f24a0166c029528d
Alexander Heinlein <alexander.heinlein@uni-koeln.de>
2021-01-04 11:02:08 +0100

    horizontalOrder

--- assets/js/mansory.js
+++ assets/js/mansory.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
   // Init Masonry
   var $grid = $('.grid').masonry({
     gutter: 10,
-    horizontalOrder: true,
+    // horizontalOrder: true,
     itemSelector: '.grid-item',
   });
   // Layout Masonry after each image loads
